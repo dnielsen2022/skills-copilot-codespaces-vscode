b24a77bfa26bfd619a7605c9bd74550db9ea3c88
Derek Nielsen <99437618+dnielsen2022@users.noreply.github.com>
2023-05-05 14:05:46 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,64 @@
+// Create web server
+// 1. Load express module
+const express = require("express");
+// 2. Create an express app
+const app = express();
+// 3. Load the comments.json file
+const comments = require("./comments.json");
+// 4. Create a route handler for GET /comments
+app.get("/comments", function (req, res) {
+  res.send(comments);
+});
+// 5. Start the server
+app.listen(3000, () => console.log("Server is listening on port 3000..."));
+
+// Path: comments.js
+// Create web server
+// 1. Load express module
+const express = require("express");
+// 2. Create an express app
+const app = express();
+// 3. Load the comments.json file
+const comments = require("./comments.json");
+// 4. Create a route handler for GET /comments
+app.get("/comments", function (req, res) {
+  res.send(comments);
+});
+// 5. Start the server
+app.listen(3000, () => console.log("Server is listening on port 3000..."));
+
+// Path: comments.js
+// Create web server
+// 1. Load express module
+const express = require("express");
+// 2. Create an express app
+const app = express();
+// 3. Load the comments.json file
+const comments = require("./comments.json");
+// 4. Create a route handler for GET /comments
+app.get("/comments", function (req, res) {
+  res.send(comments);
+});
+// 5. Start the server
+app.listen(3000, () => console.log("Server is listening on port 3000..."));
+
+// Path: comments.js
+// Create web server
+// 1. Load express module
+const express = require("express");
+// 2. Create an express app
+const app = express();
+// 3. Load the comments.json file
+const comments = require("./comments.json");
+// 4. Create a route handler for GET /comments
+app.get("/comments", function (req, res) {
+  res.send(comments);
+});
+// 5. Start the server
+app.listen(3000, () => console.log("Server is listening on port 3000..."));
+
+// Path: comments.js
+// Create web server
+// 1. Load express module
+const express = require("express");
+// 2. Create an express app
